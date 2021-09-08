db5db3c09b8baecdecd144eac3b83fdbdf26a9ab
clamdad <6515522+clamdad@users.noreply.github.com>
2021-09-08 09:53:40 -0400

    New datasource for Web/JSON Price API

--- src/telliot/datafeed/data_source.py
+++ src/telliot/datafeed/data_source.py
@@ -1,6 +1,11 @@
 from abc import abstractmethod
 from dataclasses import dataclass
+from dataclasses import field
 from typing import Any
+from typing import List
+from typing import Optional
+
+import requests
 
 
 @dataclass
@@ -10,11 +15,11 @@ class DataSource:
     A DataSource provides an input to a `DataFeed` algorithm
     """
 
-    #: Data source identifier
-    id: [str, None] = None
+    #: Unique data source identifier
+    id: str = ""
 
     #: Descriptive name
-    name: [str, None] = None
+    name: str = ""
 
     @abstractmethod
     def fetch(self) -> Any:
@@ -25,3 +30,54 @@ class DataSource:
             TODO: Handle exceptions
         """
         raise NotImplementedError
+
+
+@dataclass
+class WebJsonPriceApi(DataSource):
+    """Web JSON Price API
+
+    A price API accessible through HTTP that returns a JSON dict result.
+
+    """
+
+    #: Asset ID
+    asset_id: str = ""
+
+    #: API URL
+    url: str = ""
+
+    #: Web request timeout
+    timeout: float = 5.0
+
+    #: Dict keywords used to parse API result to get price
+    keywords: List[str] = field(default_factory=list)
+
+    async def fetch(self) -> Optional[float]:
+        """Fetch Data
+
+        Returns:
+            Data returned from source or None if an exception occurred
+        """
+        with requests.Session() as s:
+            try:
+                response = s.get(self.url, timeout=self.timeout)
+            except Exception as e:
+                msg = "API Error ({})\n{}".format(self.name, str(e))
+                print(msg)
+                return None
+
+            try:
+                d = response.json()
+            except Exception as e:
+                msg = "API Error ({}) returned invalid JSON string\n{}".format(
+                    self.name, str(e)
+                )
+                print(msg)
+                return None
+
+            for keyword in self.keywords:
+                d = d[keyword]
+
+        price = float(d)
+
+        return price
