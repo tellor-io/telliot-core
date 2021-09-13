890ce3b6e283c5ef26ea1b6b2d192bd22c286142
oraclown <owen.burton13@gmail.com>
2021-09-13 10:38:03 -0700

    code format, install deps for type checks

--- src/telliot/datafeed/schemas.py
+++ src/telliot/datafeed/schemas.py
@@ -4,28 +4,30 @@ This module builds Pydantic models used for
 enforcing data types when creating and reading
 with the datafeed api.
 """
+import datetime
 
 from pydantic import BaseModel
-import datetime
 
 
 class DataIn(BaseModel):
-    '''Ingested data model.
+    """Ingested data model.
 
     Standardize data ingested to database using typing.
     Ingested data does not include auto-generated ids.
-    '''
+    """
+
     name: str
     value: str
     timestamp: datetime.datetime
 
 
 class Data(BaseModel):
-    '''Retrieved data model.
+    """Retrieved data model.
 
     Standardize data queried from the database using typing.
     Retrieved data includes the auto-generated id for the row.
-    '''
+    """
+
     id: int
     name: str
     value: str
