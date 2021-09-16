f649a39917e89f9541b82fc4d5bf350dabc11bfc
oraclown <owen.burton13@gmail.com>
2021-09-16 13:37:49 -0700

    add basic cli & run btc/usd reporter

--- src/telliot/cli.py
+++ src/telliot/cli.py
@@ -1,12 +1,32 @@
 """ Telliot CLI
 
-A simple interface for interacting with telliot's datafeeder
-and submitter modules.
+A simple interface for interacting with telliot's functionality. 
+Configure telliot's settings via this interface's command line flags
+or in the configuration file.
 """
 
+import click
+from .btc_price_reporter import btc_reporter
+from .registry import data_feeds
 
+
+@click.group()
 def main() -> None:
+    '''Run the CLI.'''
     pass
+
+
+@main.command()
+def report() -> None:
+    '''Report data to Tellor oracle.'''
+    click.echo('Reporting data to Tellor oracle.')
+    btc_reporter.run()
+
+
+@main.command()
+def status() -> None:
+    '''Print state & configurations of current telliot client.'''
+    click.echo('State/configuration of current telliot client.')
 
 
 if __name__ == "__main__":
