a14ea5fa37cfe08ddc350dee84a922f46f315a62
oraclown <owen.burton13@gmail.com>
2021-09-22 08:29:45 -0700

    fix tox style env errors

--- src/telliot/reporter_plugins/rinkeby_btc_usd/reporter.py
+++ src/telliot/reporter_plugins/rinkeby_btc_usd/reporter.py
@@ -8,9 +8,9 @@ from typing import Any
 
 from dotenv import find_dotenv
 from dotenv import load_dotenv
+from telliot.reporter_base import Reporter
 from telliot.reporter_plugins.rinkeby_btc_usd.abi import tellorX_playground_abi
 from telliot.reporter_plugins.rinkeby_btc_usd.registry import btc_usd_data_feeds
-from telliot.reporter_base import Reporter
 from telliot.submitter.submitter_base import Submitter
 from web3 import Web3
 
