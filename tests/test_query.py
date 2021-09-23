073713c160b7e50cb34f963ad6c1ffc1e910c702
clamdad <6515522+clamdad@users.noreply.github.com>
2021-09-23 09:40:17 -0400

    Update test_query.py

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,27 +1,26 @@
 import pytest
-
 from telliot.query import PriceQuery
 from telliot.query import PriceType
 from telliot.query import QueryRegistry
 
 
 def test_oracle_price_query():
-    q = PriceQuery('eth', 'usd', PriceType.current, 1)
+    q = PriceQuery("eth", "usd", PriceType.current, 1)
     assert q.uid == "current-price-eth-in-usd"
-    assert q.question == 'What is the current price of ETH in USD?'
+    assert q.question == "What is the current price of ETH in USD?"
 
 
 def test_registry():
     qr = QueryRegistry(_queries={})
 
-    q1 = PriceQuery('eth', 'usd', PriceType.current, 1)
-    q2 = PriceQuery('btc', 'usd', PriceType.current, 2)
+    q1 = PriceQuery("eth", "usd", PriceType.current, 1)
+    q2 = PriceQuery("btc", "usd", PriceType.current, 2)
 
     qr.register(q1)
     qr.register(q2)
 
     # Demonstrate getting query by UID
-    query = qr.queries['current-price-eth-in-usd']
+    query = qr.queries["current-price-eth-in-usd"]
     assert query is q1
 
     # Demonstrate getting query by Request ID
@@ -30,12 +29,12 @@ def test_registry():
 
     # Key error
     with pytest.raises(KeyError):
-        query = qr.queries['does-not-exist']
+        query = qr.queries["does-not-exist"]
 
     # Avoid duplicate request IDs
     with pytest.raises(ValueError):
-        qr.register(PriceQuery('btc', 'usd', PriceType.current, 2))
+        qr.register(PriceQuery("btc", "usd", PriceType.current, 2))
 
     # Avoid duplicate UIDs
     with pytest.raises(ValueError):
-        qr.register(PriceQuery('btc', 'usd', PriceType.current, 3))
+        qr.register(PriceQuery("btc", "usd", PriceType.current, 3))
