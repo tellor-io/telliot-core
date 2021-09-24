a0b40703ba109c4e380f0dc32b6039c26a0f66eb
tallywiesenberg <t.j.wies@gmail.com>
2021-09-24 10:53:43 -0400

    rpc endpoint and its tests

--- src/telliot/utils/eth_utils.py
+++ src/telliot/utils/eth_utils.py
@@ -1,3 +1,4 @@
+from os import stat
 from typing import Any
 from eth_typing import abi
 from pydantic.dataclasses import dataclass
@@ -6,22 +7,28 @@ from web3 import Web3
 import web3
 
 @dataclass
-class RPCEndpoint(BaseModel):
+class RPCEndpoint:
     """Convenience wrapper for connecting to Ethereum blockchain"""
 
-    #: RPC url
-    rpc_endpoint: str
+    node_url:str
 
-    #: Tellor Contract
-    tellor_contract: web3.eth.Eth.contract
+    def connect(self):
+        self.web3 = Web3(Web3.HTTPProvider(self.node_url))
 
-    def __init__(self, rpc_endpoint:str) -> None:
-        Web3(Web3.HTTPProvider(rpc_endpoint))
+@dataclass
+class Contract:
+    """Convenience wrapper for connecting to an Ethereum contract"""
 
-    def connect_to_contract(address:str) -> web3.eth.Eth.contract:
-        """Connect to Tellor contract at a given address"""
+    #: RPCNode connection to Ethereum network
+    node: RPCEndpoint
 
-        with open("abi.json") as f:
-            abi = f.read()
+    address: str
 
-        return Web3.eth.contract(address=address, abi=abi)
+    #: Abi specifications of contract, likely loaded from JSON
+    abi: str
+
+    def connect(self):
+        self.web3 = self.node.connection.eth.contract(
+            self.address,
+            self.abi
+        )
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,6 +2,7 @@ import os
 
 from dotenv import load_dotenv
 import pytest
+import requests
 
 from telliot.utils.eth_utils import RPCEndpoint
 
@@ -10,12 +11,28 @@ load_dotenv() #we will replace this with loading from config
 def test_rpc_endpoint():
     url = os.getenv("NODE_URL")
     endpt = RPCEndpoint(url)
+    endpt.connect()
+    assert endpt.web3.eth.blockNumber > 1
     
 
-def test_bad_rpc_url():
-    url = "hi"
-    with pytest.raises(Exception) as e_info:
-        endpt = RPCEndpoint(url)
+def test_very_bad_rpc_url():
+    url = "this is not a valid rpc url"
+    endpt = RPCEndpoint(url)
+    endpt.connect()
+    #expect bad url error from requests library
+    with pytest.raises(requests.exceptions.MissingSchema) as e_info:
+        endpt.web3.eth.blockNumber
+
+def test_incomplete_rpc_url():
+    url = "https://eth-rinkeby.gateway.pokt.network/v1/lb/"
+    endpt = RPCEndpoint(url)
+    endpt.connect()
+    #expect bad url error from requests library
+    with pytest.raises(requests.exceptions.HTTPError) as e_info:
+        endpt.web3.eth.blockNumber
+
+def test_contract_connection():
+    pass
 
 
 def test_load_from_config():
