992cebc13d65b6120917c2a62fc2a1121db74b52
clamdad <6515522+clamdad@users.noreply.github.com>
2021-09-28 12:35:19 -0400

    app cleanup

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,13 @@
+""" Test application utilities
+
+"""
 import pathlib
 
 from telliot.utils.app import default_homedir
 
 
 def test_homedir():
-    """ Test default home directory """
+    """Test default home directory"""
     hd = default_homedir()
     assert isinstance(hd, pathlib.Path)
     assert hd.exists()
