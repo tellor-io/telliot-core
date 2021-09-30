e59e724a77196728371fd95e465d686b3c26ca56
oraclown <owen.burton13@gmail.com>
2021-09-29 18:15:26 -0700

    fix tox style errors

--- tests/test_contract.py
+++ tests/test_contract.py
@@ -1,9 +1,7 @@
 """
 Test covering Pytelliot EVM contract connection utils.
 """
-
 import web3
-
 from telliot.utils.contract import Contract
 from telliot.utils.rpc_endpoint import RPCEndpoint
 
@@ -13,6 +11,7 @@ with open("abi.json") as f:
 network = "mainnet"
 provider = "pokt"
 
+
 def test_read_tellor_playground():
     """Contract object should access Tellor Playground functions"""
     contract = connect_to_contract("0x4699845F22CA2705449CFD532060e04abE3F1F31")
@@ -20,6 +19,8 @@ def test_read_tellor_playground():
     assert isinstance(
         contract.web3_contract.all_functions()[0], web3.contract.ContractFunction
     )
+
+
 def connect_to_contract(address):
     """Helper function for connecting to a contract at an address"""
     url = "https://mainnet.infura.io/v3/1a09c4705f114af2997548dd901d655b"
