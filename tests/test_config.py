bd4f2aaf00bc1dcccbc99a4b03805ea9b6edefcd
clamdad <6515522+clamdad@users.noreply.github.com>
2021-10-01 08:54:58 -0400

    Style updates

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,28 +1,28 @@
 import os
+
 from telliot.utils.config import ConfigOptions
 from telliot.utils.config import LogLevel
 
 
 def test_config_options():
     c = ConfigOptions()
-    c.to_file('temp.yaml')
+    c.to_file("temp.yaml")
 
 
 def test_config_constructor():
-    """ Test Constructor
-    """
+    """Test Constructor"""
     cfg = ConfigOptions()
     # state = cfg.__getstate__()
-    assert cfg.loglevel.name == 'INFO'
+    assert cfg.loglevel.name == "INFO"
 
 
 def test_store_and_load_file():
     # Create a configuration and store it to a file
     cfg = ConfigOptions(loglevel=LogLevel.CRITICAL)
-    _ = cfg.to_file('tempfile.yaml')
+    _ = cfg.to_file("tempfile.yaml")
 
     # Class constructor from file
-    cfg2 = ConfigOptions.from_file('tempfile.yaml')
+    cfg2 = ConfigOptions.from_file("tempfile.yaml")
     assert cfg.loglevel == cfg2.loglevel
 
-    os.remove('tempfile.yaml')
+    os.remove("tempfile.yaml")
