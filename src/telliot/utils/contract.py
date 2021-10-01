03e028a75387736de6ba9cca3390946b8b78160e
tallywiesenberg <t.j.wies@gmail.com>
2021-10-01 14:43:14 -0400

    contract extension + tellorx init

--- src/telliot/utils/contract.py
+++ src/telliot/utils/contract.py
@@ -1,10 +1,11 @@
 """
 Utils for connecting to an EVM contract
 """
-from typing import Any
+from typing import Any, Callable, Tuple
 from typing import Dict
 from typing import List
 from typing import Optional
+import requests
 
 import web3
 from telliot.utils.base import Base
@@ -23,8 +24,8 @@ class Contract(Base):
     #: ABI specifications of contract
     abi: List[Dict[str, Any]]
 
-    #: Contract address
-    web3_contract: Optional[web3.contract.Contract]
+    #: web3 contract object
+    contract: Optional[web3.contract.Contract]
 
     class Config:
         arbitrary_types_allowed = True
@@ -32,8 +33,69 @@ class Contract(Base):
     def connect(self) -> None:
         """Connect to EVM contract through an RPC Endpoint"""
         if self.node.web3 is None:
-            raise TypeError("node is not connected")
+            print("node is not instantiated")
         else:
-            self.web3_contract = self.node.web3.eth.contract(
+            if not self.node.connect():
+                print("node is not connected")
+            self.contract = self.node.web3.eth.contract(
                 address=self.address, abi=self.abi
             )
+
+    def read(self,func_name:str, **kwargs:Any) -> Tuple[Tuple, bool]: 
+        """
+        Reads data from contract
+        inputs:
+        func_name (str): name of contract function to call
+
+        returns:
+        Tuple: contains contract function outputs (tuple) and success (bool)
+        """
+
+        try:
+            contract_function = self.contract.get_function_by_name(func_name)
+            return (contract_function(**kwargs).call(), True)
+        except ValueError:
+            print(f"function '{func_name}' not found in contract abi")
+            return ((), False)
+
+    def write(self, func_name:str, **kwargs:Any) -> bool:
+        """
+        Writes data to contract
+        inputs:
+        func_name (str): name of contract function to call
+
+        returns:
+        bool: success
+        """
+        try:
+
+            acc_nonce = self.node.web3.eth.get_transaction_count(self.acc.address)
+            req = requests.get("https://ethgasstation.info/json/ethgasAPI.json")
+            prices = json.loads(req.content)
+            gas_price = str(prices["fast"])
+            print("retrieved gas price:", gas_price)
+
+            print(f"View reported data: https://rinkeby.etherscan.io/tx/{tx_hash.hex()}")
+            contract_function = self.contract.get_function_by_name(func_name)
+            tx = contract_function(**kwargs).build_transaction(
+                {
+                "nonce": acc_nonce,
+                "gas": 4000000,
+                "gasPrice": self.w3.toWei(gas_price, "gwei"),
+                "chainId": self.chain_id,
+            }
+            )
+
+            tx_signed = self.acc.sign_transaction(tx)
+
+            tx_hash = self.w3.eth.send_raw_transaction(tx_signed.rawTransaction)
+
+            _ = self.w3.eth.wait_for_transaction_receipt(tx_hash, timeout=360)
+        except Exception:
+            pass
+
+
+
+    def listen(self):
+        """Listens for contract events"""
+        pass
---
+++ src/telliot/utils/tellorx.py
@@ -0,0 +1,8 @@
+"""
+Wrapper of TellorX with read and write functionality
+"""
+
+from telliot.utils.contract import Contract
+
+class TellorX(Contract):
+    pass
