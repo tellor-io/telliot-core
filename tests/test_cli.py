5c567cab5dd340afc3a047f85f298b38dd166107
oraclown <owen.burton13@gmail.com>
2021-10-02 16:03:27 -0700

    fix tox style failures

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,12 +1,12 @@
-from telliot.cli import main
 from click.testing import CliRunner
+from telliot.cli import main
 
 
 def test_status_cmd():
     """Test telliot CLI."""
     runner = CliRunner()
-    result = runner.invoke(main, ['status'])
+    result = runner.invoke(main, ["status"])
     expected_output = "State/configuration of current telliot client."
-    
+
     assert result.exit_code == 0
     assert expected_output in result.output
