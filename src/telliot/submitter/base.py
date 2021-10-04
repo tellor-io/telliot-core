db0e77f44b3050413c5adb4201a8a404ba135035
oraclown <owen.burton13@gmail.com>
2021-10-04 15:26:50 -0700

    get secrets and configs from passed ConfigOptions

--- src/telliot/submitter/base.py
+++ src/telliot/submitter/base.py
@@ -4,20 +4,92 @@ This module's provides a base Submitter class to be subclassed
 in the `reporter_plugins` module. Submitters submit off-chain data on-chain
 to the Tellor oracle.
 """
+import json
 from abc import ABC
-from abc import abstractmethod
 from typing import Any
+from typing import List
+from typing import Mapping
+
+import requests
+from telliot.utils.abi import tellor_playground_abi
+from telliot.utils.config import ConfigOptions
+from telliot.utils.rpc_endpoint import RPCEndpoint
+from web3 import Web3
 
 
 class Submitter(ABC):
-    """Base class for a Submitter.
+    """Submits BTC on testnet.
 
-    A Submitter provides a standard interface for the `telliot`
-    CLI to interact with and submit off-chain data to the
-    Tellor oracle."""
+    Submits BTC price data in USD to the TellorX playground
+    on the Rinkeby test network."""
 
-    @abstractmethod
-    async def submit_data(self, value: Any, request_id: str) -> Any:
-        """Submit latest values for each datafeed to the Tellor oracle."""
+    def __init__(self, config: ConfigOptions, abi: List[Mapping[str, Any]]) -> None:
+        """Reads user private key and node endpoint from `.env` file to
+        set up `Web3` client for interacting with the TellorX playground
+        smart contract."""
+        self.config = config
 
-        raise NotImplementedError
+        self.endpt = RPCEndpoint(
+            network=config.network, provider=config.provider, url=config.node_url
+        )
+
+        self.endpt.connect()
+
+        self.acc = self.endpt.web3.eth.account.from_key(config.private_key)
+
+        self.contract = self.endpt.web3.eth.contract(
+            self.config.contract_address, abi=tellor_playground_abi
+        )
+
+    def tobytes(self, value: int) -> Any:
+        """Casts value as a bytes array."""
+        return Web3.toBytes(hexstr=Web3.toHex(text=str(value)))
+
+    def build_tx(self, value: float, request_id: str, gas_price: str) -> Any:
+        """Assembles needed transaction data."""
+
+        # request_id_bytes = self.tobytes32(request_id)
+        value_bytes = self.tobytes(int(value * 1e6))
+        nonce = self.contract.functions.getNewValueCountbyRequestId(
+            request_id  # request_id_bytes
+        ).call()
+
+        print("nonce:", nonce)
+
+        acc_nonce = self.endpt.web3.eth.get_transaction_count(self.acc.address)
+
+        transaction = self.contract.functions.submitValue(
+            request_id, value_bytes, nonce
+        )
+
+        estimated_gas = transaction.estimateGas()
+        print("estimated gas:", estimated_gas)
+
+        built_tx = transaction.buildTransaction(
+            {
+                "nonce": acc_nonce,
+                "gas": estimated_gas,
+                "gasPrice": self.endpt.web3.toWei(gas_price, "gwei"),
+                "chainId": self.config.chain_id,
+            }
+        )
+
+        return built_tx
+
+    def submit_data(self, value: float, request_id: str) -> Any:
+        """Submits data on-chain & provides a link to view the
+        successful transaction."""
+
+        req = requests.get("https://ethgasstation.info/json/ethgasAPI.json")
+        prices = json.loads(req.content)
+        gas_price = str(prices[self.config.gasprice_speed])
+        print("retrieved gas price:", gas_price)
+
+        tx = self.build_tx(value, request_id, gas_price)
+
+        tx_signed = self.acc.sign_transaction(tx)
+
+        tx_hash = self.endpt.web3.eth.send_raw_transaction(tx_signed.rawTransaction)
+
+        _ = self.endpt.web3.eth.wait_for_transaction_receipt(tx_hash, timeout=360)
+        print(f"View reported data: https://rinkeby.etherscan.io/tx/{tx_hash.hex()}")
