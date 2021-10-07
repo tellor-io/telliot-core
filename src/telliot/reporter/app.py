a4bdd03da33bb12bcb9df6462d7c22ef91be595a
oraclown <owen.burton13@gmail.com>
2021-10-07 00:22:13 -0700

    delete old files, remove reporter app subclass

--- src/telliot/reporter/app.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-
-from telliot.reporter.config import ReporterConfig
-from telliot.utils.app import Application
-
-
-class ReporterApplication(Application):
-    config: Optional[ReporterConfig]
