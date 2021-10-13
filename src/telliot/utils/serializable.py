7d6ea2d6cb5091b76aa9066750dbc4408e6c5c09
clamdad <6515522+clamdad@users.noreply.github.com>
2021-10-13 09:40:59 -0400

    Improved class serialization

--- src/telliot/utils/serializable.py
+++ src/telliot/utils/serializable.py
@@ -7,22 +7,37 @@ from typing import Type
 from pydantic import BaseModel
 
 
-class SerializableSubclassModel(BaseModel):
-    """A helper subclass that allows nested serialization of subclasses"""
+class SerializableModel(BaseModel):
+    """A helper subclass that allows nested serialization
 
-    #: Container to register subclasses for pydantic export hack (see below)
-    _subtypes_: ClassVar[Dict[str, Type[BaseModel]]] = dict()
+    The serialized format contains the class name, which can be used
+    to reconstruct an object of the correct class.
 
-    # used to register automatically all the submodels in `_subtypes_`.
+    A registry is maintained to provide import context for
+    reconstruction from the class name string.
+    """
+
+    #: Type Registry
+    _registry_: ClassVar[Dict[str, Type[BaseModel]]] = dict()
+
     def __init_subclass__(cls, type: Optional[str] = None) -> None:
-        cls._subtypes_[type or cls.__name__] = cls
+        """Register all subclasses"""
+        cls._registry_[type or cls.__name__] = cls
 
     @classmethod
     def __get_validators__(cls) -> Any:
-        yield cls._convert_to_real_type_
+        yield cls._convert_to_model
 
     @classmethod
-    def _convert_to_real_type_(cls, data: Any) -> BaseModel:
+    def _convert_to_model(cls, data: Any) -> BaseModel:
+        """Convert input to a class instance
+
+        When input is a JSON string, it should have two attributes:
+
+        - 'type': The class name
+        - 'inputs': keyword arguments to pass to the constructor
+
+        """
 
         if isinstance(data, BaseModel):
             return data
@@ -32,13 +47,29 @@ class SerializableSubclassModel(BaseModel):
         if data_type is None:
             raise ValueError("Missing 'type'")
 
-        sub = cls._subtypes_.get(data_type)
+        factory = cls._registry_.get(data_type)
 
-        if sub is None:
-            raise TypeError(f"Unsupported sub-type: {data_type}")
+        if factory is None:
+            raise TypeError(f"Unsupported type: {data_type}")
 
-        return sub(**data)
+        inputs = data.get("inputs")
+
+        if inputs is None:
+            raise ValueError("Missing inputs")
+
+        return factory(**inputs)
 
     @classmethod
     def parse_obj(cls, obj: Dict[str, Any]) -> Any:
-        return cls._convert_to_real_type_(obj)
+        return cls._convert_to_model(obj)
+
+    def dict(self, **d_args: Any) -> Dict[str, Any]:
+        """Convert model to dict
+
+        Override pydantic model to return a dict that includes
+        the class name to help deserialization.
+        """
+
+        dr = {"type": self.__class__.__name__, "inputs": super().dict(**d_args)}
+
+        return dr
