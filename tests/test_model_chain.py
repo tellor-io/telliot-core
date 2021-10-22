c20d1971349b8875844d30464208c80463c107a3
clamdad <6515522+clamdad@users.noreply.github.com>
2021-10-22 09:44:52 -0400

    Style fix

--- tests/test_model_chain.py
+++ tests/test_model_chain.py
@@ -1,21 +1,22 @@
-from telliot.model.chain import ChainList
 from telliot.model.chain import Chain
+from telliot.model.chain import ChainList
 
 
 def test_chain():
 
     cl = ChainList()
-    ch = cl.get_chain('ETH', 'mainnet')
+    ch = cl.get_chain("ETH", "mainnet")
     assert isinstance(ch, Chain)
-    assert ch.chain == 'ETH'
+    assert ch.chain == "ETH"
     assert ch.chain_id == 1
-    assert ch.network == 'mainnet'
+    assert ch.network == "mainnet"
+
 
 def test_matic():
     cl = ChainList()
-    ch = cl.get_chain('MATIC', 'mainnet')
+    ch = cl.get_chain("MATIC", "mainnet")
     assert isinstance(ch, Chain)
     assert ch.chain_id == 137
 
-    ch = cl.get_chain('MATIC', 'testnet')
+    ch = cl.get_chain("MATIC", "testnet")
     assert ch.chain_id == 80001
