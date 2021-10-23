735a9f4175c1557d297761aed3aff3e9ff578712
oraclown <owen.burton13@gmail.com>
2021-10-23 15:41:57 +0200

    fix tox errors, refactor

--- src/telliot/utils/contract.py
+++ src/telliot/utils/contract.py
@@ -13,6 +13,7 @@ import requests
 import web3
 from eth_typing.evm import ChecksumAddress
 from telliot.apps.telliot_config import TelliotConfig
+from telliot.model.endpoints import RPCEndpoint
 from telliot.utils.base import Base
 from telliot.utils.response import ResponseStatus
 from web3 import Web3
@@ -21,9 +22,6 @@ from web3.datastructures import AttributeDict
 
 class Contract(Base):
     """Convenience wrapper for connecting to an Ethereum contract"""
-
-    #: RPCNode connection to Ethereum network
-    # node: RPCEndpoint
 
     #: Contract address
     address: Union[str, ChecksumAddress]
@@ -37,27 +35,29 @@ class Contract(Base):
     #: global pytelliot configurations
     config: TelliotConfig
 
+    #: RPCNode connection to Ethereum network
+    node: Optional[RPCEndpoint]
+
     def connect(self) -> ResponseStatus:
         """Connect to EVM contract through an RPC Endpoint"""
-        if self.config.get_endpoint() is None:
-            msg = "node not configurated"
-            return ResponseStatus(ok=False, error_msg=msg)
+        self.node = self.config.get_endpoint()
 
-        elif self.config.get_endpoint().web3 is None:
-            msg = "node is not instantiated"
+        if not self.node:
+            msg = "node not configured"
             return ResponseStatus(ok=False, error_msg=msg)
-
         else:
-            if not self.config.get_endpoint().connect():
-                msg = "node is not connected"
+            if not self.node.web3:
+                msg = "node is not instantiated"
                 return ResponseStatus(ok=False, error_msg=msg)
-            self.address = Web3.toChecksumAddress(self.address)
-            self.contract = self.config.get_endpoint().web3.eth.contract(
-                address=self.address, abi=self.abi
-            )
-            return ResponseStatus(ok=True)
 
-    def read(self, func_name: str, **kwargs: Any) -> Tuple[ResponseStatus, Optional[Tuple[Any]]]:
+        self.node.connect()
+        self.address = Web3.toChecksumAddress(self.address)
+        self.contract = self.node.web3.eth.contract(address=self.address, abi=self.abi)
+        return ResponseStatus(ok=True)
+
+    def read(
+        self, func_name: str, **kwargs: Any
+    ) -> Tuple[ResponseStatus, Optional[Tuple[Any]]]:
         """
         Reads data from contract
         inputs:
@@ -76,12 +76,8 @@ class Contract(Base):
                 msg = f"function '{func_name}' not found in contract abi"
                 return ResponseStatus(ok=False, error=e, error_msg=msg), None
         else:
-            if self.connect():
-                msg = "now connected to contract"
-                return self.read(func_name=func_name, **kwargs)
-            else:
-                msg = "unable to connect to contract"
-                return ResponseStatus(ok=False, error_msg=msg), None
+            msg = "no instance of contract"
+            return ResponseStatus(ok=False, error_msg=msg), None
 
     def write(
         self,
@@ -92,89 +88,85 @@ class Contract(Base):
         **kwargs: Any,
     ) -> Tuple[ResponseStatus, Optional[List[AttributeDict[Any, Any]]], int]:
         """For submitting any contract transaction. Retries supported!"""
-        if self.contract:
-            try:
-                transaction_receipts = []
-                # Iterate through retry attempts
-                for _ in range(retries + 1):
-                    status = ResponseStatus()
+        if not self.contract:
+            msg = "unable to connect to contract"
+            return ResponseStatus(ok=False, error_msg=msg), None, gas_price
 
-                    # build transaction
-                    acc_nonce = (
-                        self.config.get_endpoint().web3.eth.get_transaction_count(
-                            self.config.acc.address
-                        )
-                    )
-                    contract_function = self.contract.get_function_by_name(func_name)
-                    transaction = contract_function(**kwargs)
-                    estimated_gas = transaction.estimateGas()
-                    print("estimated gas:", estimated_gas)
+        if not self.node:
+            msg = "no node instance"
+            return ResponseStatus(ok=False, error_msg=msg), None, gas_price
 
-                    built_tx = transaction.buildTransaction(
-                        {
-                            "nonce": acc_nonce,
-                            "gas": estimated_gas,
-                            "gasPrice": self.config.get_endpoint().web3.toWei(gas_price, "gwei"),
-                            "chainId": self.config.get_endpoint().chain_id,
-                        }
-                    )
+        acc = self.node.web3.eth.account.from_key(self.config.main.private_key)
 
-                    # get gas price
-                    rsp = requests.get("https://ethgasstation.info/json/ethgasAPI.json")
-                    prices = json.loads(rsp.content)
-                    gas_price = int(prices["fast"] + extra_gas_price)
+        try:
+            transaction_receipts = []
+            # Iterate through retry attempts
+            for _ in range(retries + 1):
+                status = ResponseStatus()
 
-                    # submit transaction
-                    tx_signed = self.config.acc.address.sign_transaction(built_tx)
+                # build transaction
+                acc_nonce = self.node.web3.eth.get_transaction_count(acc.address)
+                contract_function = self.contract.get_function_by_name(func_name)
+                transaction = contract_function(**kwargs)
+                estimated_gas = transaction.estimateGas()
+                print("estimated gas:", estimated_gas)
 
-                    tx_hash = self.config.get_endpoint().web3.eth.send_raw_transaction(
-                        tx_signed.rawTransaction
-                    )
-
-                    # Confirm transaction
-                    tx_receipt = self.config.get_endpoint().web3.eth.wait_for_transaction_receipt(
-                        tx_hash, timeout=360
-                    )
-
-                    # Point to relevant explorer
-                    print(
-                        f"View reported data: {self.config.get_endpoint().explorer}{tx_hash.hex()}"
-                    )
-
-                    # Exit loop if transaction successful
-                    if tx_receipt and status.ok:
-                        transaction_receipts.append(tx_receipt)
-                        return status, transaction_receipts, gas_price
-                    elif (
-                        not status.ok
-                        and status.error
-                        and "replacement transaction underpriced" in status.error
-                    ):
-                        extra_gas_price += gas_price
-                    else:
-                        extra_gas_price = 0
-
-                    status.ok = False
-                    status.error = "ran out of retries, tx unsuccessful"
-
-                    return status, transaction_receipts, gas_price
-            except Exception as e:
-                status.ok = False
-                status.error = str(e.args)
-                status.e = e
-                return status, None, gas_price
-        else:
-            if self.connect():
-                msg = "now connected to contract"
-                return self.write(
-                    func_name=func_name,
-                    gas_price=gas_price,
-                    extra_gas_price=extra_gas_price,
-                    **kwargs,
+                built_tx = transaction.buildTransaction(
+                    {
+                        "nonce": acc_nonce,
+                        "gas": estimated_gas,
+                        "gasPrice": self.node.web3.toWei(gas_price, "gwei"),
+                        "chainId": self.node.chain_id,
+                    }
                 )
-            else:
-                msg = "unable to connect to contract"
-                return ResponseStatus(ok=False, error_msg=msg), None, gas_price
+
+                # get gas price
+                rsp = requests.get("https://ethgasstation.info/json/ethgasAPI.json")
+                prices = json.loads(rsp.content)
+                gas_price = int(prices["fast"] + extra_gas_price)
+
+                # submit transaction
+                tx_signed = acc.address.sign_transaction(built_tx)
+
+                tx_hash = self.node.web3.eth.send_raw_transaction(
+                    tx_signed.rawTransaction
+                )
+
+                # Confirm transaction
+                tx_receipt = self.node.web3.eth.wait_for_transaction_receipt(
+                    tx_hash, timeout=360
+                )
+
+                # Point to relevant explorer
+                print(
+                    f"""View reported data: \n
+                    {self.node.explorer}{tx_hash.hex()}
+                    """
+                )
+
+                # Exit loop if transaction successful
+                if tx_receipt and status.ok:
+                    transaction_receipts.append(tx_receipt)
+                    return status, transaction_receipts, gas_price
+                elif (
+                    not status.ok
+                    and status.error
+                    and "replacement transaction underpriced" in status.error
+                ):
+                    extra_gas_price += gas_price
+                else:
+                    extra_gas_price = 0
+
+            status.ok = False
+            status.error = "ran out of retries, tx unsuccessful"
+
+            return status, transaction_receipts, gas_price
+
+        except Exception as e:
+            status.ok = False
+            status.error = str(e.args)
+            status.e = e
+            return status, None, gas_price
 
     def listen(self) -> None:
         """Wrapper for listening for contract events"""
