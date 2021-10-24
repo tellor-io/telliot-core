fe78d4535a6459c81eb1114b817835eb36b723d3
clamdad <6515522+clamdad@users.noreply.github.com>
2021-10-24 06:33:16 -0400

    Style fixes

--- src/telliot/plugin.py
+++ src/telliot/plugin.py
@@ -1,12 +1,9 @@
-
-
 import importlib
 import pkgutil
 
 
 telliot_plugins = {
     name: importlib.import_module(name)
-    for finder, name, ispkg
-    in pkgutil.iter_modules()
-    if name.startswith('telliot_')
+    for finder, name, ispkg in pkgutil.iter_modules()
+    if name.startswith("telliot_")
 }
--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -3,4 +3,4 @@ from telliot.plugin import telliot_plugins
 
 def test_discovered_plugins():
 
-    assert 'telliot_examples' in telliot_plugins
+    assert "telliot_examples" in telliot_plugins
