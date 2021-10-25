9e8fc62b5f8e8a631a741c1c05eaccdd5f7723f9
tallywiesenberg <t.j.wies@gmail.com>
2021-10-24 22:03:26 -0400

    temporarily sync

--- src/telliot/contract/gas.py
+++ src/telliot/contract/gas.py
@@ -1,4 +1,4 @@
-import asyncio
+# import asyncio
 import json
 from typing import Literal
 
@@ -7,15 +7,23 @@ import requests
 ethgastypes = Literal["fast", "fastest", "safeLow", "average"]
 
 
-async def estimate_gas() -> int:
-    """Estimate current ETH gas price
+# async def estimate_gas() -> int:
+#     """Estimate current ETH gas price
 
-    Work In Progress - Just do something quick
-    """
-    return asyncio.run(ethgasstation("fast"))
+#     Work In Progress - Just do something quick
+#     """
+#     return asyncio.run(ethgasstation("fast"))
 
 
-async def ethgasstation(style: ethgastypes = "fast") -> int:
+# async def ethgasstation(style: ethgastypes = "fast") -> int:
+#     """Fetch gas price from ethgasstation"""
+#     rsp = requests.get("https://ethgasstation.info/json/ethgasAPI.json")
+#     prices = json.loads(rsp.content)
+#     gas_price = int(prices[style])
+
+#     return gas_price
+
+def estimate_gas(style: ethgastypes = "fast") -> int:
     """Fetch gas price from ethgasstation"""
     rsp = requests.get("https://ethgasstation.info/json/ethgasAPI.json")
     prices = json.loads(rsp.content)
