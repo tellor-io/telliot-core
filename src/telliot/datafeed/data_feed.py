976766b260248bb570fe36f66c86ec8392205f95
clamdad <6515522+clamdad@users.noreply.github.com>
2021-11-03 11:41:52 -0400

    Datafeed now contains a source, doesn't inherit.

--- src/telliot/datafeed/data_feed.py
+++ src/telliot/datafeed/data_feed.py
@@ -1,20 +1,37 @@
 """ telliot.datafeed.data_feed
 
 """
-from abc import ABC
 from dataclasses import dataclass
-
+from telliot.model.base import Base
 from telliot.datafeed.data_source import DataSource
 from telliot.queries.query import OracleQuery
+from typing import TypeVar, Generic
+
+T = TypeVar("T")
 
 
 @dataclass
-class DataFeed(DataSource, ABC):
-    """Data feed
+class DataFeed(Generic[T], Base):
+    """ Data feed providing query response
 
-    A data feed is a DataSource that creates a response value for an
-    `OracleQuery`.
+    A data feed contains a DataSource to fetch values in response to an `OracleQuery`.
     """
+    source: DataSource[T]
 
     #: Query supported by this data feed
     query: OracleQuery
+
+
+if __name__ == '__main__':
+    # Example:
+    from telliot.datafeed.data_source import RandomSource
+    from telliot.queries.legacy_query import LegacyRequest
+
+    feed = DataFeed(
+        source=RandomSource(),
+        query=LegacyRequest(legacy_id=4)
+    )
+
+    import yaml
+
+    print(yaml.dump(feed.get_state(), sort_keys=False))
--- tests/test_data_feed.py
+++ tests/test_data_feed.py
@@ -1,42 +1,20 @@
 """ Simple example of creating a "plug-in" data feed
 
 """
-from dataclasses import dataclass
-from dataclasses import field
-from typing import Any
-from typing import Optional
-
-import pytest
-from telliot.answer import TimeStampedAnswer
 from telliot.datafeed.data_feed import DataFeed
 from telliot.datafeed.data_source import RandomSource
 from telliot.queries.legacy_query import LegacyRequest
 
-# from telliot.datafeed.data_source import SourceOutputType
+
+def test_data_feed():
+    """Test a new feed"""
+    myfeed = DataFeed(
+        source=RandomSource(),
+        query=LegacyRequest(legacy_id=4)
+    )
+
+    import yaml
+
+    print(yaml.dump(myfeed.get_state(), sort_keys=False))
 
 
-@dataclass
-class MyDataFeed(DataFeed):
-    random_source: RandomSource = field(default_factory=RandomSource)
-
-    async def update_value(self) -> Optional[TimeStampedAnswer[Any]]:
-        # async def update_value(self) -> SourceOutputType:
-
-        self._value = await self.random_source.update_value()
-
-        return self.value
-
-
-my_feed = MyDataFeed(query=LegacyRequest(legacy_id=4))
-
-
-@pytest.mark.asyncio
-async def test_my_data_source():
-
-    # result, value, tstamp = await my_feed.update_value()
-    tsval = await my_feed.update_value()
-    # assert result.ok
-    assert 0 <= tsval.val < 1
-
-    print(my_feed.get_state())
-    print(my_feed.value)
