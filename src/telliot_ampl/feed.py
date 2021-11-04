3ee65c6b0e08c98536d0b40cc65c2f51760252da
oraclown <owen.burton13@gmail.com>
2021-11-04 11:50:14 +0000

    add ampl query & datafeed

--- src/telliot_ampl/feed.py
+++ src/telliot_ampl/feed.py
@@ -1,2 +1,26 @@
 """Example datafeed used by AMPLUSDVWAPReporter."""
 
+import statistics
+
+from telliot_ampl.sources import AnyBlockSource, BraveNewCoinSource
+from telliot.queries.coin_price import CoinPrice
+from telliot.datafeed.data_feed import DataFeed
+from telliot.datafeed.pricing.price_feed import AggregatePriceSource
+
+
+
+data_sources = [
+    AnyBlockSource(),
+    BraveNewCoinSource()
+]
+
+
+ampl_usd_vwap_feed = DataFeed(
+    query=CoinPrice(coin="ampl", currency="usd", price_type="vwap"),
+    source=AggregatePriceSource(
+        sources=data_sources, 
+        asset="ampl", 
+        currency="usd", 
+        algorithm=statistics.median
+    ),
+)
