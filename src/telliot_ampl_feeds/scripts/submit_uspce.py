dbde18f4a97533aef85681e5bcbb2d38e947c3ac
oraclown <owen.burton13@gmail.com>
2021-11-08 18:14:53 +0000

    refactor, add docstrings, typing

--- src/telliot_ampl_feeds/scripts/submit_uspce.py
+++ src/telliot_ampl_feeds/scripts/submit_uspce.py
@@ -1,22 +1,16 @@
-'''Submits three month rolling average of the USPCE to TellorX on Rinkeby.'''
-import web3
-from hexbytes.main import HexBytes
-from telliot.contract.gas import fetch_gas_price
-from telliot.queries.legacy_query import LegacyRequest
-from web3 import Web3
-import os
+"""Submits three month rolling average of the USPCE to TellorX on Rinkeby."""
+import asyncio
+from typing import Optional
 
-import pytest
 from telliot.apps.telliot_config import TelliotConfig
 from telliot.contract.contract import Contract
+from telliot.contract.gas import fetch_gas_price
+from telliot.queries.legacy_query import LegacyRequest
 from telliot.utils.abi import rinkeby_tellor_master
 from telliot.utils.abi import rinkeby_tellor_oracle
-from dotenv import load_dotenv
-import asyncio
 
-load_dotenv()
 
-def get_cfg():
+def get_cfg() -> TelliotConfig:
     """Get rinkeby endpoint from config
 
     If environment variables are defined, they will override the values in config files
@@ -26,29 +20,22 @@ def get_cfg():
     # Override configuration for rinkeby testnet
     cfg.main.chain_id = 4
 
-    rinkeby_endpoint = cfg.get_endpoint()
-    # assert rinkeby_endpoint.network == "rinkeby"
-
-    # Optionally override private key and URL with ENV vars for testing
-    if os.getenv("PRIVATE_KEY", None):
-        # cfg.main.private_key = os.environ["PRIVATE_KEY"]
-        cfg.main.private_key = os.getenv("PRIVATE_KEY")
-
-
-    if os.getenv("NODE_URL", None):
-        # rinkeby_endpoint.url = os.environ["NODE_URL"]
-        rinkeby_endpoint.url = os.getenv("NODE_URL")
+    _ = cfg.get_endpoint()
 
     return cfg
 
 
-def get_master(cfg):
+def get_master(cfg: TelliotConfig) -> Optional[Contract]:
     """Helper function for connecting to a contract at an address"""
     endpoint = cfg.get_endpoint()
+    if not endpoint:
+        print("Could not connect to master contract.")
+        return None
+
     endpoint.connect()
     master = Contract(
         address="0x657b95c228A5de81cdc3F85be7954072c08A6042",
-        abi=rinkeby_tellor_master,
+        abi=rinkeby_tellor_master,  # type: ignore
         node=endpoint,
         private_key=cfg.main.private_key,
     )
@@ -56,13 +43,18 @@ def get_master(cfg):
     return master
 
 
-def get_oracle(cfg):
+def get_oracle(cfg: TelliotConfig) -> Optional[Contract]:
     """Helper function for connecting to a contract at an address"""
     endpoint = cfg.get_endpoint()
-    endpoint.connect()
+    if not endpoint:
+        print("Could not connect to master contract.")
+        return None
+
+    if endpoint:
+        endpoint.connect()
     oracle = Contract(
         address="0x07b521108788C6fD79F471D603A2594576D47477",
-        abi=rinkeby_tellor_oracle,
+        abi=rinkeby_tellor_oracle,  # type: ignore
         node=endpoint,
         private_key=cfg.main.private_key,
     )
@@ -70,8 +62,9 @@ def get_oracle(cfg):
     return oracle
 
 
-def parse_user_val():
-    print('Enter USPCE value (example: 13659.3:')
+def parse_user_val() -> int:
+    """Parse USPCE value from user input."""
+    print("Enter USPCE value (example: 13659.3:")
 
     uspce = None
 
@@ -79,40 +72,45 @@ def parse_user_val():
         inpt = input()
 
         try:
-            inpt = int(float(inpt) * 1000000)
+            inpt = int(float(inpt) * 1000000)  # type: ignore
         except ValueError:
-            print('Invalid input. Enter int or float.')
+            print("Invalid input. Enter int or float.")
             continue
 
-        _ = input(f'Submitting value: {inpt}\nPress [ENTER] to confirm.')
+        _ = input(f"Submitting value: {inpt}\nPress [ENTER] to confirm.")
 
         uspce = inpt
 
-    # print success/fail & rinkebyscan link
-    # print(f'Success. See here: {uspce}')
     return uspce
 
 
-async def submit():
+async def submit() -> None:
+    """Submit USPCE value to TellorX oracle."""
     cfg = get_cfg()
+    if not cfg:
+        print("Error getting default configs.")
+        return
+
     master = get_master(cfg)
     oracle = get_oracle(cfg)
 
-    gas_price =  await fetch_gas_price()  # TODO clarify gas price units
+    if not master or not oracle:
+        return
+
+    gas_price = await fetch_gas_price()  # TODO clarify gas price units
     user = master.node.web3.eth.account.from_key(cfg.main.private_key).address
-    print(user)
+    print('User:', user)
 
     balance, status = await master.read("balanceOf", _user=user)
-    print(balance / 1e18)
+    print('Current balance:', balance / 1e18)  # type: ignore
 
     is_staked, status = await master.read("getStakerInfo", _staker=user)
     print(is_staked)
 
-    if is_staked[0] == 0:
+    if is_staked is not None and is_staked[0] == 0:
         _, status = await master.write_with_retry(
             func_name="depositStake", gas_price=gas_price, extra_gas_price=20, retries=2
         )
-        assert status.ok
 
     q = LegacyRequest(legacy_id=41)
     usr_input = parse_user_val()
@@ -124,9 +122,6 @@ async def submit():
     value_count, status = await oracle.read(
         func_name="getTimestampCountById", _queryId=query_id
     )
-    assert status.ok
-    assert value_count >= 0
-    print(value_count)
 
     _, status = await oracle.write_with_retry(
         func_name="submitValue",
@@ -139,9 +134,8 @@ async def submit():
         _queryData=query_data,
     )
 
-    print(status.error)
-    assert status.ok
+    return status
 
 
 if __name__ == "__main__":
-    asyncio.run(submit())
+    _ = asyncio.run(submit())
