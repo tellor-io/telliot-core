6bb712bbac2b4e4ecd7f8ce513ac6efa01e791e4
clamdad <6515522+clamdad@users.noreply.github.com>
2021-11-15 10:20:15 -0500

    Update test fixture to use new directory

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,9 +4,7 @@ import os
 import pytest
 from telliot_core.apps.telliot_config import TelliotConfig
 from telliot_core.contract.contract import Contract
-from telliot_core.utils.abi import rinkeby_tellor_master
-from telliot_core.utils.abi import rinkeby_tellor_oracle
-
+from telliot_core.directory.tellorx import TellorDirectory
 
 @pytest.fixture(scope="session", autouse=True)
 def rinkeby_cfg():
@@ -35,11 +33,13 @@ def rinkeby_cfg():
 @pytest.fixture(scope="session")
 def master(rinkeby_cfg):
     """Helper function for connecting to a contract at an address"""
+    rinkeby_master_abi = TellorDirectory[4]["master"]["abi"]
+    rinkeby_master_address = TellorDirectory[4]["master"]["address"]
     endpoint = rinkeby_cfg.get_endpoint()
     endpoint.connect()
     master = Contract(
-        address="0x657b95c228A5de81cdc3F85be7954072c08A6042",
-        abi=rinkeby_tellor_master,
+        address=rinkeby_master_address,  # "0x657b95c228A5de81cdc3F85be7954072c08A6042",
+        abi=rinkeby_master_abi,
         node=endpoint,
         private_key=rinkeby_cfg.main.private_key,
     )
@@ -50,11 +50,13 @@ def master(rinkeby_cfg):
 @pytest.fixture(scope="session", autouse=True)
 def oracle(rinkeby_cfg):
     """Helper function for connecting to a contract at an address"""
+    rinkeby_oracle_abi = TellorDirectory[4]["oracle"]["abi"]
+    rinkeby_oracle_address = TellorDirectory[4]["oracle"]["address"]
     endpoint = rinkeby_cfg.get_endpoint()
     endpoint.connect()
     oracle = Contract(
-        address="0x07b521108788C6fD79F471D603A2594576D47477",
-        abi=rinkeby_tellor_oracle,
+        address=rinkeby_oracle_address, # "0x07b521108788C6fD79F471D603A2594576D47477",
+        abi=rinkeby_oracle_abi,
         node=endpoint,
         private_key=rinkeby_cfg.main.private_key,
     )
