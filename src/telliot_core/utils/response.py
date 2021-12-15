5586a5216ba8f3f7e35bc274a7add2d97745dca4
clamdad <6515522+clamdad@users.noreply.github.com>
2021-12-15 12:25:37 -0500

    New helper function for error response logging

--- src/telliot_core/utils/response.py
+++ src/telliot_core/utils/response.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Callable
 from typing import Optional
 
 from telliot_core.model.base import Base
@@ -9,3 +10,27 @@ class ResponseStatus(Base):
     ok: bool = True
     error: Optional[str] = None
     e: Optional[Exception] = None
+
+
+def error_status(
+    note: str,
+    e: Optional[Exception] = None,
+    log: Optional[Callable[[str], None]] = None,
+) -> ResponseStatus:
+    """Helper for reporting and logging error status"""
+
+    status = ResponseStatus()
+    status.ok = False
+
+    if e:
+        msg = f"{note}: {repr(e)}"
+    else:
+        status.e = e
+        msg = f"{note}"
+
+    status.error = str(msg)
+
+    if log:
+        log(msg)
+
+    return status
