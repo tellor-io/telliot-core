9db4ea37f17a3e364c59ec0b21e22897931697f8
clamdad <6515522+clamdad@users.noreply.github.com>
2021-12-16 07:21:59 -0500

    Style fix

--- scripts/export_query_catalog.py
+++ scripts/export_query_catalog.py
@@ -1,8 +1,7 @@
 from telliot_core.queries.query_catalog import query_catalog
 
-with open('query_catalog.md', 'w') as f:
+with open("query_catalog.md", "w") as f:
     f.write(query_catalog.to_markdown())
 
-with open('query_catalog.yaml', 'w') as f:
+with open("query_catalog.yaml", "w") as f:
     f.write(query_catalog.to_yaml())
-
