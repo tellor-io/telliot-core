058fb17a20dc401211547c5909d8806129c62e53
clamdad <6515522+clamdad@users.noreply.github.com>
2022-01-04 13:41:24 -0400

    Add CLI for query suggestion.

--- src/telliot_core/reporters/reporter_utils.py
+++ src/telliot_core/reporters/reporter_utils.py
@@ -1,12 +1,11 @@
+from typing import List
 from typing import Optional
 
-from telliot_core.data.query_catalog import query_catalog
-from telliot_core.queries import OracleQuery
 from telliot_core.tellorx.oracle import TellorxOracleContract
 
 # List of currently active reporters
 
-reporter_sync_schedule = [
+reporter_sync_schedule: List[str] = [
     "eth-usd-legacy",
     "btc-usd-legacy",
     "ampl-legacy",
@@ -17,7 +16,7 @@ reporter_sync_schedule = [
 
 async def tellorx_suggested_report(
     oracle: TellorxOracleContract,
-) -> Optional[OracleQuery]:
+) -> Optional[str]:
     """Returns the currently suggested query to report against.
 
     The suggested query changes each time a block contains a query response.
@@ -29,19 +28,9 @@ async def tellorx_suggested_report(
 
     if status.ok:
         suggested_idx = timestamp.ts % len(reporter_sync_schedule)
-
-        suggested_query_tag = reporter_sync_schedule[suggested_idx]
-
-        entries = query_catalog.find(tag=suggested_query_tag)
-        if len(entries) == 0:
-            print(f"Unknown query tag: {suggested_query_tag}.")
-            return None
-        else:
-            catalog_entry = entries[0]
-            # Get the query object from the catalog entry
-            q = catalog_entry.query
-            assert isinstance(q, OracleQuery)
-            return q
+        suggested_qtag = reporter_sync_schedule[suggested_idx]
+        assert isinstance(suggested_qtag, str)
+        return suggested_qtag
 
     else:
         return None
--- tests/test_reporter_utils.py
+++ tests/test_reporter_utils.py
@@ -1,6 +1,9 @@
 import pytest
+from click.testing import CliRunner
 
+import telliot_core.cli.main
 from telliot_core.apps.core import TelliotCore
+from telliot_core.data.query_catalog import query_catalog
 from telliot_core.queries import OracleQuery
 from telliot_core.reporters.reporter_utils import tellorx_suggested_report
 
@@ -8,5 +11,19 @@ from telliot_core.reporters.reporter_utils import tellorx_suggested_report
 @pytest.mark.asyncio
 async def test_suggested_report(rinkeby_cfg):
     async with TelliotCore(config=rinkeby_cfg) as core:
-        q = await tellorx_suggested_report(core.tellorx.oracle)
+        qtag = await tellorx_suggested_report(core.tellorx.oracle)
+        assert isinstance(qtag, str)
+        entries = query_catalog.find(tag=qtag)
+        assert len(entries) == 1
+        catalog_entry = entries[0]
+        q = catalog_entry.query
         assert isinstance(q, OracleQuery)
+
+
+def test_suggested_report_cli():
+    """Test suggested report CLI"""
+    runner = CliRunner()
+    result = runner.invoke(
+        telliot_core.cli.main.main, ["--test_config", "query", "suggest"]
+    )
+    assert "Suggested query" in result.output
