6e4397646065a63d5d532021153cca0e78e13034
clamdad <6515522+clamdad@users.noreply.github.com>
2022-01-18 16:38:35 -0500

    Rewrote directory module for multi-chain support.

--- tests/test_directory.py
+++ tests/test_directory.py
@@ -1,15 +1,40 @@
-from telliot_core.directory.tellorx import tellor_directory
+from telliot_core.directory import ContractDirectory
+from telliot_core.directory import ContractInfo
+from telliot_core.directory import directory_config_file
 
 
-def test_telliot_directory():
-
-    tellor_contracts = tellor_directory.find(org="tellor")
-    assert len(tellor_contracts) == 8
-
-    rinkeby_contracts = tellor_directory.find(org="tellor", chain_id=4)
-    assert len(rinkeby_contracts) == 4
-
-    tellor_master_rinkeby = tellor_directory.find(
-        org="tellor", name="master", chain_id=4
+def test_contract_info():
+    c = ContractInfo(
+        org="tellor",
+        name="tellorx-master",
+        address={
+            1: "0x88dF592F8eb5D7Bd38bFeF7dEb0fBc02cf3778a0",
+            4: "0x88dF592F8eb5D7Bd38bFeF7dEb0fBc02cf3778a0",
+        },
+        abi_file="tellorx-master-abi.json",
     )
-    assert len(tellor_master_rinkeby) == 1
+
+    # Make sure we can get ABI
+    assert isinstance(c.abi, list)
+
+
+def test_contract_directory():
+    cd = ContractDirectory()
+    assert len(cd.entries) == 6
+
+    master = cd.find(name="tellorx-master")[0]
+    assert isinstance(master.abi, list)
+
+    treasury = cd.find(address="0x2dB91443f2b562B8b2B2e8E4fC0A3EDD6c195147")[0]
+    assert isinstance(treasury.abi, list)
+
+    tellorx = cd.find(name="tellorx")
+    assert len(tellorx) == 5
+
+    mainnet_contracts = cd.find(chain_id=1)
+    assert isinstance(mainnet_contracts[0], ContractInfo)
+
+
+def test_directory_config_file():
+    cd = directory_config_file().get_config()
+    assert len(cd.entries) == 6
