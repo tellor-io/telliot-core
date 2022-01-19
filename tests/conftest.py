c43a9280e74750102bcb8713b4c78e80c09a6cba
clamdad <6515522+clamdad@users.noreply.github.com>
2022-01-19 12:56:20 -0500

    Add test fixture for mumbai

--- tests/conftest.py
+++ tests/conftest.py
@@ -34,3 +34,27 @@ def rinkeby_cfg():
         rinkeby_staker.address = "0x8D8D2006A485FA4a75dFD8Da8f63dA31401B8fA2"
 
     return cfg
+
+
+@pytest.fixture(scope="session", autouse=True)
+def mumbai_cfg():
+    """Return a test telliot configuration for use on polygon-mumbai
+
+    If environment variables are defined, they will override the values in config files
+    """
+    cfg = TelliotConfig()
+
+    # Override configuration for rinkeby testnet
+    cfg.main.chain_id = 80001
+
+    # Replace staker private key
+    if os.getenv("PRIVATE_KEY", None):
+        private_key = os.environ["PRIVATE_KEY"]
+        rinkeby_stakers = cfg.stakers.find(chain_id=4)
+        if len(rinkeby_stakers) == 0:
+            raise Exception("No staker/private key defined for rinkeby")
+        rinkeby_staker = rinkeby_stakers[0]
+        rinkeby_staker.private_key = private_key
+        rinkeby_staker.address = "0x8D8D2006A485FA4a75dFD8Da8f63dA31401B8fA2"
+
+    return cfg
--- tests/test_tellorflex_polygon.py
+++ tests/test_tellorflex_polygon.py
@@ -3,20 +3,19 @@ import pytest
 from telliot_core.apps.core import TelliotCore
 from telliot_core.utils.timestamp import TimeStamp
 
-# CID = 137
-CID = 80001
-
 
 @pytest.mark.asyncio
-async def test_main():
-    async with TelliotCore(chain_id=CID) as core:
+async def test_main(mumbai_cfg):
+    async with TelliotCore(config=mumbai_cfg) as core:
+
+        chain_id = core.config.main.chain_id
 
         flex = core.get_tellorflex_contracts()
 
         governance_address = await flex.oracle.get_governance_address()
-        if CID == 137:
+        if chain_id == 137:
             assert governance_address == "0x2cFC5bCE14862D46fBA3bb46A36A8b2d7E4aC040"
-        elif CID == 80001:
+        elif chain_id == 80001:
             assert governance_address == "0x0Fe623d889Ad1c599E5fF3076A57D1D4F2448CDe"
 
         stake_amount = await flex.oracle.get_stake_amount()
@@ -31,9 +30,9 @@ async def test_main():
         print(lock)
 
         token_address = await flex.oracle.get_token_address()
-        if CID == 137:
+        if chain_id == 137:
             assert token_address == "0xE3322702BEdaaEd36CdDAb233360B939775ae5f1"
-        elif CID == 80001:
+        elif chain_id == 80001:
             assert token_address == "0x002E861910D7f87BAa832A22Ac436F25FB66FA24"
 
         total_stake = await flex.oracle.get_total_stake_amount()
