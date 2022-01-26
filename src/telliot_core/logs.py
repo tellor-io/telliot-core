fa3474b0b5a658941b1343df8e5fbfae1cc92cea
clamdad <6515522+clamdad@users.noreply.github.com>
2022-01-26 09:01:40 -0500

    Reduce logging noise by attaching handlers to telliot logger instead of root logger

--- src/telliot_core/logs.py
+++ src/telliot_core/logs.py
@@ -12,23 +12,22 @@ def init_logging(level: int) -> logging.Logger:
     log_dir = default_homedir() / "logs"
     log_dir.mkdir(exist_ok=True)
     log_filename = log_dir / ("telliot_core.log")
-    log_format = "%(levelname)-7s | %(message)s"
-
-    root = logging.getLogger()
-    root.setLevel(level)
+    log_format = "%(levelname)-7s | %(name)s | %(message)s"
     formatter = logging.Formatter(log_format)
+
+    log_core.setLevel(logging.DEBUG)
 
     # Log to file
     one_mb = 1024 * 1024
     fh = RotatingFileHandler(log_filename, maxBytes=one_mb * 5, backupCount=5)
     fh.setLevel(logging.DEBUG)
     fh.setFormatter(formatter)
-    root.addHandler(fh)
+    log_core.addHandler(fh)
 
     # Log to stdout
     stream = logging.StreamHandler(sys.stdout)
     stream.setLevel(level)
     stream.setFormatter(formatter)
-    root.addHandler(stream)
+    log_core.addHandler(stream)
 
     return log_core
