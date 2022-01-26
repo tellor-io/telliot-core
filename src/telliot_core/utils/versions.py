27aa01cb55ba650ff50cf7295a72233d7d9ad6d5
clamdad <6515522+clamdad@users.noreply.github.com>
2022-01-26 09:45:04 -0500

    Auto-configure logging for plugins

--- src/telliot_core/utils/versions.py
+++ src/telliot_core/utils/versions.py
@@ -8,10 +8,9 @@ from telliot_core.plugin.discover import telliot_plugins
 def show_telliot_versions(caller: Callable[[Any], None] = print, include_plugins: bool = True) -> None:
     caller(f"telliot-core {telliot_core.__version__}")
     if include_plugins:
-        if len(telliot_plugins) > 1:
-            for name, pkg in telliot_plugins.items():
-                if name != "telliot_core":
-                    try:
-                        caller(f"{name} (plugin): Version {pkg.__version__}")
-                    except AttributeError:
-                        caller(f"{name} (plugin): Version UNKNOWN")
+        for name, pkg in telliot_plugins.items():
+            if name != "telliot_core":
+                try:
+                    caller(f"{name} (plugin): Version {pkg.__version__}")
+                except AttributeError:
+                    caller(f"{name} (plugin): Version UNKNOWN")
