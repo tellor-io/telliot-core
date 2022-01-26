83330b2f99dce006e4aae3abe27c2e3cfac750c9
clamdad <6515522+clamdad@users.noreply.github.com>
2022-01-26 17:00:06 -0500

    Style fix

--- tests/test_json_query.py
+++ tests/test_json_query.py
@@ -6,4 +6,4 @@ def test_data_interface():
     query_data = b'{"type":"SpotPrice","asset":"btc","currency":"usd"}'
     q = JsonQuery.get_query_from_data(query_data)
     assert isinstance(q, SpotPrice)
-    assert q.asset == 'btc'
+    assert q.asset == "btc"
