a36846c8de467ffab331764cb18cb1e2fee2a1c2
oraclown <owen.burton13@gmail.com>
2022-01-27 14:10:49 -0800

    Standardize style and typing

--- src/telliot_core/data/aws.py
+++ src/telliot_core/data/aws.py
@@ -72,19 +72,19 @@ def print_all_zones() -> None:
     Prerequisits: Install and configure AWS CLI: http://aws.amazon.com/cli/
 
     Source: https://stelligent.com/2014/05/02/list-all-the-availability-zones/"""
-    ec2 = boto3.client('ec2')
+    ec2 = boto3.client("ec2")
     # Retrieves all regions/endpoints that work with EC2
     aws_regions = ec2.describe_regions()
     # Get a list of regions and then instantiate a new ec2 client for
     # each region in order to get list of AZs for the region
-    for region in aws_regions['Regions']:
-        my_region_name = region['RegionName']
-        ec2_region = boto3.client('ec2', region_name=my_region_name)
-        my_region = [{'Name': 'region-name', 'Values': [my_region_name]}]
+    for region in aws_regions["Regions"]:
+        my_region_name = region["RegionName"]
+        ec2_region = boto3.client("ec2", region_name=my_region_name)
+        my_region = [{"Name": "region-name", "Values": [my_region_name]}]
         # print ("Current Region is %s" % my_region_name)
         aws_azs = ec2_region.describe_availability_zones(Filters=my_region)
-        for az in aws_azs['AvailabilityZones']:
-            zone = az['ZoneName']
+        for az in aws_azs["AvailabilityZones"]:
+            zone = az["ZoneName"]
             print(zone)
 
 
--- src/telliot_core/queries/price/aws_spot_price.py
+++ src/telliot_core/queries/price/aws_spot_price.py
@@ -1,23 +1,38 @@
 import logging
 from dataclasses import dataclass
 from typing import Any
+from typing import Optional
 
+from telliot_core.data.aws import INSTANCE_TYPES
+from telliot_core.data.aws import ZONES
 from telliot_core.dtypes.float_type import UnsignedFloatType
 from telliot_core.dtypes.value_type import ValueType
 from telliot_core.queries.abi_query import AbiQuery
+from telliot_core.queries.query_abis import PARAMETERS_ABI_LOOKUP
 
 logger = logging.getLogger(__name__)
 
 
 @dataclass
-class AWSSpotPrice(AbiQuery):
-    """Returns the spot price in USD of the hourly rate to rent AWS compute instances."""
+class AwsSpotPrice(AbiQuery):
+    """Returns the spot price in USD of the hourly rate to rent AWS compute instances.
 
-    parameters: list[dict[str, Any]]
+    Attributes:
+
+        zone: Asset ID (see data specifications for a full list of supported assets)
+
+        instance: Currency (default = `usd`)
+
+        abi:
+    """
+
+    zone: Optional[str] = None
+    instance: Optional[str] = None
+    params_abi: Optional[list[dict[str, Any]]] = None  # type: ignore
 
     @property
     def value_type(self) -> ValueType:
-        """Data type returned for a SpotPrice query.
+        """Data type returned for a AwsSpotPrice query.
 
         - `ufixed256x18`: 256-bit unsigned integer with 18 decimals of precision
         - `packed`: false
@@ -25,5 +40,20 @@ class AWSSpotPrice(AbiQuery):
         return UnsignedFloatType(abi_type="ufixed256x18", packed=False)
 
     def __post_init__(self) -> None:
-        assert self.parameters[0]["name"] == "zone"
-        assert self.parameters[1]["name"] == "instance"
+        self.params_abi = PARAMETERS_ABI_LOOKUP["AwsSpotPrice"]
+
+        if self.zone is None or self.zone == "":
+            self.zone = self.params_abi[0]["value"]
+        else:
+            self.params_abi[0]["value"] = self.zone
+
+        if self.instance is None or self.instance == "":
+            self.instance = self.params_abi[1]["value"]
+        else:
+            self.params_abi[1]["value"] = self.instance
+
+        if self.zone not in ZONES:
+            raise ValueError(f"AWS zone ({self.zone}) not found")
+
+        if self.instance not in INSTANCE_TYPES:
+            raise ValueError(f"Instance type ({self.instance}) not found")
