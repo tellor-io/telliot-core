5646c79f69c6515c996791e9088759201109eaba
oraclown <owen.burton13@gmail.com>
2022-01-27 06:43:54 -0800

    Implement new query type

--- src/telliot_core/queries/abi_query.py
+++ src/telliot_core/queries/abi_query.py
@@ -1,26 +1,31 @@
+from typing import Any
+
+from eth_abi import encode_abi
+
 from telliot_core.queries.query import OracleQuery
 
 
 class AbiQuery(OracleQuery):
     """An Oracle Query that uses ABI-encoding to compute the query_data."""
 
+    parameters: list[dict[str, Any]]
+
     @property
     def query_data(self) -> bytes:
-        """Encode the query `descriptor` to create the query `data` field for
-        use in the ``TellorX.Oracle.tipQuery()`` contract call.
+        """Encode the query type and parameters to create the query data.
 
-        This method uses ABI encoding to encode the values in the Query Descriptor.
+        This method uses ABI encoding to encode the query's parameter values.
         A valid JSON ABI specification is required to perform the encoding.
         (see https://docs.soliditylang.org/en/v0.5.3/abi-spec.html).
-
-        TODO: Implement for AbiQuery
         """
-        raise NotImplementedError
+
+        param_types = [p["type"] for p in self.parameters]
+        param_values = [p["value"] for p in self.parameters]
+        encoded_params = encode_abi(param_types, param_values)
+
+        return encode_abi(["string", "bytes"], [self.type, encoded_params])
 
     @staticmethod
     def get_query_from_data(query_data: bytes) -> OracleQuery:
-        """Recreate an oracle query from `query_data`
-
-        TODO: Implement for AbiQuery
-        """
-        raise NotImplementedError
+        # Implemented in telliot_core.queries.abi_queries
+        pass
