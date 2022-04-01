ea50e904746bfb40cc533d8e65fccd92f3eef718
oraclown <owen.burton13@gmail.com>
2022-04-01 13:51:21 -0400

    Get size of example submitted val

---
+++ scripts/morphware_data_sizes.py
@@ -0,0 +1,54 @@
+"""Print the size of the data returned by a Morphware query (data reported)."""
+import sys
+import requests
+import json
+from telliot_core.queries.morphware import Morphware
+
+
+def main():
+    """
+    Print the size of the data returned by a Morphware query
+    using Morphare-provided data source endpoint:
+
+    curl --request POST http://167.172.239.133:5000/products -H "Content-Type: application/json" \
+    -d '{"provider":"amazon","service":"compute","region":"us-east-1"}'
+
+    Example:
+    $ python scripts/morphware_data_sizes.py
+    """
+    # Retrieve data from source provided by Morphware
+    headers = {
+        # Already added when you pass json=
+        # 'Content-Type': 'application/json',
+    }
+    json_data = {
+        'provider': 'amazon',
+        'service': 'compute',
+        'region': 'us-east-1',
+    }
+    rsp = requests.post('http://167.172.239.133:5000/products', headers=headers, json=json_data)
+    d = json.loads(rsp.text)
+    unfiltered_data = d["products"]
+    print("Num products:", len(unfiltered_data))
+
+    # Remove unneeded product data
+    data = []
+    for product in unfiltered_data:
+        data.append(json.dumps({
+            "instanceType": unfiltered_data[0]["type"],
+            "numCPUs": str(unfiltered_data[0]["cpusPerVm"]),
+            "RAM": str(unfiltered_data[0]["memPerVm"]),
+            "onDemandPricePerHour": str(unfiltered_data[0]["onDemandPrice"]),
+        }))
+    print("Expample filtered data:")
+    print(json.dumps(data[:2], indent=4))
+    
+    # Get size of encoded string[]
+    q = Morphware(version=1)
+    submit_value = q.value_type.encode(data)
+    print(f"Size of data being reported: {sys.getsizeof(submit_value)} bytes")
+    # print(submit_value.hex())
+
+
+if __name__ == "__main__":
+    main()
