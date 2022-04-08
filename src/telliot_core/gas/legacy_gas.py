5c22a741cfa7ab329de30d7114702e0d1d422f8f
akrem <akremstudy@gmail.com>
2022-04-08 06:49:22 -0400

    retry api for non response

--- src/telliot_core/gas/legacy_gas.py
+++ src/telliot_core/gas/legacy_gas.py
@@ -1,12 +1,17 @@
 import json
+import logging
+from json.decoder import JSONDecodeError
+from time import sleep
 from typing import Literal
+from typing import Optional
 
 import requests
 
+logger = logging.getLogger(__name__)
 ethgastypes = Literal["fast", "fastest", "safeLow", "average"]
 
 
-async def fetch_gas_price() -> int:
+async def fetch_gas_price() -> Optional[int]:
     """Estimate current ETH gas price
 
     Current implementation fetches from ethgasstation
@@ -17,10 +22,18 @@ async def fetch_gas_price() -> int:
     return await ethgasstation("fast")
 
 
-async def ethgasstation(style: ethgastypes = "fast") -> int:
+async def ethgasstation(style: ethgastypes = "fast") -> Optional[int]:
     """Fetch gas price from ethgasstation in gwei"""
-    rsp = requests.get("https://ethgasstation.info/json/ethgasAPI.json")
-    prices = json.loads(rsp.content)
-    gas_price = int(prices[style])
-
-    return int(gas_price / 10)  # json output is gwei*10
+    i = 0
+    while i < 2:
+        try:
+            rsp = requests.get("https://ethgastation.info/json/ethgasAPI.json")
+            prices = json.loads(rsp.content)
+            gas_price = int(prices[style])
+            return int(gas_price / 10)  # json output is gwei*10
+        except JSONDecodeError:
+            logger.info("ethgasstation api retrying ...")
+            i += 1
+            sleep(1)
+            continue
+    return None
