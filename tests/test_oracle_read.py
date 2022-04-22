90123470b0a1860a44a2950d0fa7d52e53f3505c
oraclown <owen.burton13@gmail.com>
2022-04-22 13:32:54 -0400

    Use local node for all oracle read tests

--- tests/test_oracle_read.py
+++ tests/test_oracle_read.py
@@ -4,6 +4,7 @@ from brownie import TellorXOracleMock
 
 from telliot_core.apps.core import TelliotCore
 from telliot_core.tellor.tellorx.oracle import TellorxOracleContract
+from telliot_core.utils.timestamp import TimeStamp
 
 
 @pytest.fixture
@@ -28,7 +29,6 @@ async def test_getReportTimestampByIndex(rinkeby_test_cfg, tellorx_oracle_mock_c
         assert timestamp == 1234
 
 
-@pytest.mark.skip
 @pytest.mark.asyncio
 async def test_getReportingLock(rinkeby_test_cfg, tellorx_oracle_mock_contract):
     async with TelliotCore(config=rinkeby_test_cfg) as core:
@@ -39,12 +39,12 @@ async def test_getReportingLock(rinkeby_test_cfg, tellorx_oracle_mock_contract):
 
         result, status = await oracle.getReportingLock()
         assert status.ok
+        assert result == 12
         print(result)
 
 
-@pytest.mark.skip
 @pytest.mark.asyncio
-async def test_gettimebasedreward(rinkeby_test_cfg):
+async def test_gettimebasedreward(rinkeby_test_cfg, tellorx_oracle_mock_contract):
 
     async with TelliotCore(config=rinkeby_test_cfg) as core:
         account = core.get_account()
@@ -54,12 +54,11 @@ async def test_gettimebasedreward(rinkeby_test_cfg):
 
         result, status = await oracle.getTimeBasedReward()
         assert status.ok
-        print(result)
+        assert result == 1
 
 
-@pytest.mark.skip
 @pytest.mark.asyncio
-async def test_getCurrentReward(rinkeby_test_cfg):
+async def test_getCurrentReward(rinkeby_test_cfg, tellorx_oracle_mock_contract):
     async with TelliotCore(config=rinkeby_test_cfg) as core:
         account = core.get_account()
         oracle = TellorxOracleContract(core.endpoint, account)
@@ -68,30 +67,14 @@ async def test_getCurrentReward(rinkeby_test_cfg):
 
         queryId = "0x0000000000000000000000000000000000000000000000000000000000000001"
         (tips, reward), status = await oracle.getCurrentReward(queryId)
+
         assert status.ok
-        print(tips)
-        print(reward)
+        assert tips == 1
+        assert reward == 2
 
 
-@pytest.mark.skip
 @pytest.mark.asyncio
-async def test_getBlockNumberByTimestamp(rinkeby_test_cfg):
-    async with TelliotCore(config=rinkeby_test_cfg) as core:
-        account = core.get_account()
-        oracle = TellorxOracleContract(core.endpoint, account)
-        oracle.address = tellorx_oracle_mock_contract.address  # Override with locally-deployed mock contract address
-        oracle.connect()
-
-        queryId = "0x0000000000000000000000000000000000000000000000000000000000000001"
-        timestamp = 1638377944
-        result, status = await oracle.getBlockNumberByTimestamp(queryId, timestamp)
-        assert status.ok
-        print(result)
-
-
-@pytest.mark.skip
-@pytest.mark.asyncio
-async def test_getTimestampCountById(rinkeby_test_cfg):
+async def test_getTimestampCountById(rinkeby_test_cfg, tellorx_oracle_mock_contract):
     async with TelliotCore(config=rinkeby_test_cfg) as core:
         account = core.get_account()
         oracle = TellorxOracleContract(core.endpoint, account)
@@ -100,27 +83,28 @@ async def test_getTimestampCountById(rinkeby_test_cfg):
 
         queryId = "0x0000000000000000000000000000000000000000000000000000000000000001"
         result, status = await oracle.getTimestampCountById(queryId)
-        print(result)
+
         assert status.ok
-        assert result > 30
+        assert result == 30
 
 
-@pytest.mark.skip
 @pytest.mark.asyncio
-async def test_getTimeOfLastNewValue(rinkeby_test_cfg):
+async def test_getTimeOfLastNewValue(rinkeby_test_cfg, tellorx_oracle_mock_contract):
     async with TelliotCore(config=rinkeby_test_cfg) as core:
         account = core.get_account()
         oracle = TellorxOracleContract(core.endpoint, account)
         oracle.address = tellorx_oracle_mock_contract.address  # Override with locally-deployed mock contract address
         oracle.connect()
+
         result, status = await oracle.getTimeOfLastNewValue()
+
         assert status.ok
-        print(result)
+        assert isinstance(result, TimeStamp)
+        assert result.ts == 123456789
 
 
-@pytest.mark.skip
 @pytest.mark.asyncio
-async def test_getTipsById(rinkeby_test_cfg):
+async def test_getTipsById(rinkeby_test_cfg, tellorx_oracle_mock_contract):
     async with TelliotCore(config=rinkeby_test_cfg) as core:
         account = core.get_account()
         oracle = TellorxOracleContract(core.endpoint, account)
@@ -129,5 +113,6 @@ async def test_getTipsById(rinkeby_test_cfg):
 
         queryId = "0x0000000000000000000000000000000000000000000000000000000000000002"
         result, status = await oracle.getTipsById(queryId)
-        print(result)
+
         assert status.ok
+        assert result == 3
