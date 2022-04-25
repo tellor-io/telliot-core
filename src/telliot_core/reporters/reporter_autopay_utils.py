461a854e24281bd9109f9a5dbb6cdbd3a05958c4
akrem <akremstudy@gmail.com>
2022-04-24 23:19:36 -0400

    fixed listener tip calculation

--- src/telliot_core/reporters/reporter_autopay_utils.py
+++ src/telliot_core/reporters/reporter_autopay_utils.py
@@ -1,5 +1,5 @@
-from time import time
-from typing import Any
+import math
+from typing import Optional
 from typing import Tuple
 
 from eth_abi import decode_single
@@ -18,53 +18,62 @@ logger = get_logger(__name__)
 query_ids_in_catalog = {query_catalog._entries[tag].query_id: tag for tag in query_catalog._entries}
 
 
-async def single_tip_suggested_query_id(autopay: TellorFlexAutopayContract) -> Tuple[Any, Any]:
+# Mapping to feed details index
+class DetailsMap:
+    reward = 0
+    balance = 1
+    startTime = 2
+    interval = 3
+    window = 4
+    priceThreshold = 5
+    feedsWithFundingIndex = 6
+
+
+detail = DetailsMap()
+
+
+async def single_tip_suggested_query_id(autopay: TellorFlexAutopayContract) -> Tuple[Optional[str], Optional[int]]:
     """Returns the currently suggested query to report against.
 
     Pulls query_ids with tips available from the tellor autopay
     contract, checks if query ids exist in the query catalog,
     then sorts them from highest tip amount to lowest
     """
+
     assert isinstance(autopay, TellorFlexAutopayContract)
     query_id_lis, status = await autopay.read("getFundedQueryIds")
 
     if status.ok:
 
-        tips_dictionary = {i: autopay.get_current_tip(i) for i in query_id_lis if i in query_ids_in_catalog}
-        tips_sorted = sorted(tips_dictionary.items(), key=lambda item: item[1])  # type: ignore
+        tips_dictionary = {
+            i: await autopay.get_current_tip(i) for i in query_id_lis if ("0x" + i.hex()) in query_ids_in_catalog
+        }
+        tips_sorted = sorted(tips_dictionary.items(), key=lambda item: item[1])
         if tips_sorted:
-            suggested_qtag = tips_sorted[0][0]
-            suggested_qtag_tip = tips_sorted[0][1]
+            suggested_qtag = "0x" + tips_sorted[0][0].hex()
+            suggested_qtag_tip = tips_sorted[0][1][0]
             assert isinstance(suggested_qtag, str)
-            return suggested_qtag, suggested_qtag_tip
+            assert isinstance(suggested_qtag_tip, int)
+            return query_ids_in_catalog[suggested_qtag], int(suggested_qtag_tip * 1e18)
         else:
             return None, None
     else:
         msg = "can't get FundedQueryIds"
-        return error_status(note=msg, log=logger.warning)
+        error_status(note=msg, log=logger.warning)
+        return None, None
 
 
-feed_details_map = {
-    "reward": 0,
-    "balance": 1,
-    "startTime": 2,
-    "interval": 3,
-    "window": 4,
-    "priceThreshold": 5,
-    "feedsWithFundingIndex": 6,
-}
-
-
-async def get_feed_details(query_id: str, autopay: TellorFlexAutopayContract) -> Tuple[Any, Any]:
+async def get_feed_details(query_id: str, autopay: TellorFlexAutopayContract) -> Optional[int]:
     assert isinstance(autopay, TellorFlexAutopayContract)
-    current_time = TimeStamp(time()).ts
+    current_time = TimeStamp.now().ts
     feed_ids, status = await autopay.read("getCurrentFeeds", _queryId=query_id)
 
     if not status.ok:
         msg = "can't get feed details to calculate tips"
-        return None, error_status(note=msg, log=logger.warning)
+        error_status(note=msg, log=logger.warning)
+        return None
 
-    f_q_dict = {}
+    feed_query_dict = {}
     for i in feed_ids:
         if i:
             feed_id = "0x" + i.hex()
@@ -72,35 +81,38 @@ async def get_feed_details(query_id: str, autopay: TellorFlexAutopayContract) ->
 
             if not status.ok:
                 msg = "couldn't get feed details from contract"
-                return None, error_status(note=msg, log=logger.warning)
+                error_status(note=msg, log=logger.warning)
+                return None
 
-            if feed_details[feed_details_map["balance"]] <= 0:
+            if feed_details[detail.balance] <= 0 and feed_details[detail.reward] <= 0:
                 msg = f"{feed_id}, feed has no remaining balance"
-                return None, error_status(note=msg, log=logger.warning)
+                error_status(note=msg, log=logger.warning)
+                return None
 
-        n = (current_time - feed_details[feed_details_map["startTime"]]) / feed_details[feed_details_map["interval"]]
-        c = feed_details[feed_details_map["startTime"]] / feed_details[feed_details_map["interval"]] * n
-        response, status = await autopay.read("getDataBefore", _queryId=query_id, _timestamp=current_time)
+        n = math.floor(
+            (current_time - feed_details[detail.startTime]) / feed_details[detail.interval]
+        )  # round down with important
+        c = feed_details[detail.startTime] + (feed_details[detail.interval] * n)
+        response, status = await autopay.read("getCurrentValue", _queryId=query_id)
+
         if not status.ok:
-            msg = "couldn't get DataBefore"
-            return None, error_status(note=msg, log=logger.warning)
+            msg = "couldn't get value before from getCurrentValue"
+            error_status(note=msg, log=logger.warning)
+            return None
         value_before_now = (
             decode_single("(uint256)", response[1])[0] / 1e18
         )  # there might be an issue here with decimals!!!!!!!!!
         timestamp_before_now = response[2]
-        if not status.ok:
-            msg = "couldn't get value before from getDataBefore"
-            return None, error_status(note=msg, log=logger.warning)
 
-        rules = [
-            feed_details[feed_details_map["priceThreshold"]] == 0,
-            feed_details[feed_details_map["balance"]] > 0,
-            current_time - c < feed_details[feed_details_map["window"]],
-            timestamp_before_now < c,
-        ]
+        rules = [current_time - c < feed_details[detail.window], timestamp_before_now < c]
 
-        if all(rules):
-            f_q_dict[i] = feed_details[feed_details_map["reward"]]
+        if not all(rules):
+            msg = f"{query_ids_in_catalog[query_id]}, isn't eligible for a tip"
+            error_status(note=msg, log=logger.info)
+            return None
+
+        if feed_details[detail.priceThreshold] == 0:
+            feed_query_dict[i] = feed_details[detail.reward]
         else:
             datafeed = CATALOG_FEEDS[query_ids_in_catalog[query_id]]
             value_now = await datafeed.source.fetch_new_datapoint()
@@ -108,38 +120,47 @@ async def get_feed_details(query_id: str, autopay: TellorFlexAutopayContract) ->
 
             if value_before_now == 0:
                 price_change = 10000
+
             elif value_now >= value_before_now:
                 price_change = (10000 * (value_now - value_before_now)) / value_before_now
+
             else:
                 price_change = (10000 * (value_before_now - value_now)) / value_before_now
 
-            if price_change > feed_details[feed_details_map["price_threshold"]]:
-                f_q_dict[i] = feed_details[feed_details_map["reward"]]
+            if price_change > feed_details[detail.priceThreshold]:
+                feed_query_dict[i] = feed_details[detail.reward]
 
-    tips_total = sum(f_q_dict.values())
-    return query_id, tips_total
+    tips_total = sum(feed_query_dict.values())
+    if tips_total > 0:
+        logger.info(f"{query_ids_in_catalog[query_id]} has potentially {tips_total/1e18} in tips")
+
+    return tips_total
 
 
-async def autopay_suggested_report(autopay: TellorFlexAutopayContract) -> Tuple[Any, Any]:
+async def autopay_suggested_report(autopay: TellorFlexAutopayContract) -> Tuple[Optional[str], Optional[int]]:
     chain = autopay.node.chain_id
 
     if chain in (1, 4, 137, 80001):
         assert isinstance(autopay, TellorFlexAutopayContract)
-        datafeed_suggested_qtag = {j: await get_feed_details(i, autopay) for i, j in query_ids_in_catalog.items()}
-        datafeed_tips_sorted = sorted(datafeed_suggested_qtag.items(), key=lambda item: item[1][1])  # type: ignore
+        datafeed_dict = {j: await get_feed_details(i, autopay) for i, j in query_ids_in_catalog.items()}
+        datafeed_suggestion = {i: j for i, j in datafeed_dict.items() if j}
+        datafeed_tips_sorted = sorted(datafeed_suggestion.items(), key=lambda item: item[1], reverse=True)
         single_suggested_qtag, single_suggested_tip = await single_tip_suggested_query_id(autopay=autopay)
-
-        if datafeed_tips_sorted and single_suggested_qtag:
-            if datafeed_tips_sorted[-1][1][1] > single_suggested_tip:
-                return datafeed_tips_sorted[-1][0], datafeed_tips_sorted[-1][1][1]
-            else:
-                return query_ids_in_catalog[single_suggested_qtag[0]], single_suggested_tip
-        elif not datafeed_suggested_qtag and single_suggested_qtag:
-            return query_ids_in_catalog[single_suggested_qtag[0]], single_suggested_tip
-        elif datafeed_suggested_qtag and not single_suggested_qtag:
-            return datafeed_tips_sorted[-1][0], datafeed_tips_sorted[-1][1][1]
+        if datafeed_tips_sorted:
+            datafeed_suggested_qtag, datafeed_suggested_tip = datafeed_tips_sorted[0]
         else:
-            return None, None
+            return single_suggested_qtag, single_suggested_tip
+
+        if not single_suggested_qtag:
+            return datafeed_suggested_qtag, datafeed_suggested_tip
+
+        if datafeed_suggested_qtag == single_suggested_qtag:
+            return datafeed_suggested_qtag, (datafeed_suggested_tip + single_suggested_tip)  # type: ignore
+
+        elif datafeed_suggested_tip > single_suggested_tip:  # type: ignore
+            return datafeed_suggested_qtag, datafeed_suggested_tip
+        else:
+            return single_suggested_qtag, single_suggested_tip
 
     else:
         return None, None
