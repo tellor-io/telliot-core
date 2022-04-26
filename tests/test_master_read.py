9bc023ce13d9b6fa6d856ced7221d4326da06445
lauren gregory <lmg2@illinois.edu>
2022-04-26 13:39:18 -0500

    test_master_read.py switched over to local testing
    added mock master contract, switched test function over to connecting to local chain via rinkeby_test_cfg

--- tests/test_master_read.py
+++ tests/test_master_read.py
@@ -1,21 +1,30 @@
 import pytest
 
+from brownie import accounts
+from brownie import TellorXMasterMock
+
 from telliot_core.apps.core import TelliotCore
 from telliot_core.tellor.tellorx.master import account_status_map
+from telliot_core.tellor.tellorx.master import TellorxMasterContract
 from telliot_core.utils.timestamp import TimeStamp
 
+@pytest.fixture
+def tellorx_master_mock_contract():
+    return accounts[0].deploy(TellorXMasterMock)
 
 @pytest.mark.asyncio
-async def test_get_staker_info(rinkeby_cfg):
+async def test_get_staker_info(rinkeby_test_cfg, tellorx_master_mock_contract):
 
-    async with TelliotCore(config=rinkeby_cfg) as core:
-        tellorx = core.get_tellorx_contracts()
-
+    async with TelliotCore(config=rinkeby_test_cfg) as core:
         account = core.get_account()
-        address = account.address
+        tellorx = TellorxMasterContract(core.endpoint, account)
+        tellorx.address = tellorx_master_mock_contract.address
+        tellorx.connect()
 
-        result, status = await tellorx.master.getStakerInfo(address)
+        result, status = await tellorx.getStakerInfo(tellorx.address)
+
         assert status.ok
         assert len(result) == 2
         assert result[0] in account_status_map.values()
         assert isinstance(result[1], TimeStamp)
+
