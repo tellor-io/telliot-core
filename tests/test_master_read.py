d2e466ea0f2629c140edc1f8a0dfbf1b326d6c2a
lauren gregory <lmg2@illinois.edu>
2022-04-26 13:51:08 -0500

    style fixes

--- tests/test_master_read.py
+++ tests/test_master_read.py
@@ -1,5 +1,4 @@
 import pytest
-
 from brownie import accounts
 from brownie import TellorXMasterMock
 
@@ -8,9 +7,11 @@ from telliot_core.tellor.tellorx.master import account_status_map
 from telliot_core.tellor.tellorx.master import TellorxMasterContract
 from telliot_core.utils.timestamp import TimeStamp
 
+
 @pytest.fixture
 def tellorx_master_mock_contract():
     return accounts[0].deploy(TellorXMasterMock)
+
 
 @pytest.mark.asyncio
 async def test_get_staker_info(rinkeby_test_cfg, tellorx_master_mock_contract):
@@ -27,4 +28,3 @@ async def test_get_staker_info(rinkeby_test_cfg, tellorx_master_mock_contract):
         assert len(result) == 2
         assert result[0] in account_status_map.values()
         assert isinstance(result[1], TimeStamp)
-
