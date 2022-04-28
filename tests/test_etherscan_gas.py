99e26418fff52550abf8765943189fa9fdc1de0c
oraclown <owen.burton13@gmail.com>
2022-04-28 09:23:25 -0600

    Mock gas retrieval failures

--- tests/test_etherscan_gas.py
+++ tests/test_etherscan_gas.py
@@ -1,11 +1,10 @@
-# from unittest import mock
-import pytest
+from unittest import mock
 
-from telliot_core.gas import etherscan_gas
+import pytest
+import requests
+
 from telliot_core.gas.etherscan_gas import EtherscanGasPrice
 from telliot_core.gas.etherscan_gas import EtherscanGasPriceSource
-
-# import requests
 
 
 @pytest.mark.asyncio
@@ -17,27 +16,35 @@ async def test_etherscan_gas():
 
 @pytest.mark.asyncio
 async def test_etherscan_gas_error(caplog):
-    # etherscan_gas.requests.get = mock.Mock(side_effect=requests.exceptions.ConnectionError)
-    # c = EtherscanGasPriceSource()
-    # result = await c.fetch_new_datapoint()
-    # assert result[0] is None
-    # assert result[1] is None
-    # assert "Connection timeout" in caplog.text
+    def conn_timeout(url, *args, **kwargs):
+        raise requests.exceptions.ConnectTimeout()
 
-    # etherscan_gas.requests.get = mock.Mock(side_effect=requests.exceptions.Timeout)
-    # c = EtherscanGasPriceSource()
-    # result = await c.fetch_new_datapoint()
-    # assert result[0] is None
-    # assert result[1] is None
-    # assert "Timeout" in caplog.text
+    with mock.patch("requests.Session.get", side_effect=conn_timeout):
+        c = EtherscanGasPriceSource()
+        result = await c.fetch_new_datapoint()
+        assert result[0] is None
+        assert result[1] is None
+        assert "Connection timeout" in caplog.text
 
-    def invalid_json():
-        return "<p>blah<p/>"
+    def json_decode_error(*args, **kwargs):
+        raise requests.exceptions.JSONDecodeError()
 
-    etherscan_gas.requests.get = invalid_json
-    c = etherscan_gas.EtherscanGasPriceSource()
-    result = await c.fetch_new_datapoint()
-    assert result[0] is None
-    assert result[1] is None
-    # print(caplog.records)
-    # assert "JSONDecodeError" in caplog.text
+    with mock.patch("json.loads", side_effect=json_decode_error):
+        c = EtherscanGasPriceSource()
+        result = await c.fetch_new_datapoint()
+        assert result[0] is None
+        assert result[1] is None
+        assert "JSON decode error" in caplog.text
+
+    def missing_key_rsp(url, *args, **kwargs):
+        rsp = requests.Response()
+        rsp.status_code = 200
+        rsp._content = b'{"no_status_ke": 1}'
+        return rsp
+
+    with mock.patch("requests.Session.get", side_effect=missing_key_rsp):
+        c = EtherscanGasPriceSource()
+        result = await c.fetch_new_datapoint()
+        assert result[0] is None
+        assert result[1] is None
+        assert "Key error" in caplog.text
