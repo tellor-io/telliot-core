b6ee7087560e69c73551199b7bfa11e12981dee2
oraclown <owen.burton13@gmail.com>
2022-04-28 08:05:56 -0600

    Handle etherscan gas price errors

--- tests/test_etherscan_gas.py
+++ tests/test_etherscan_gas.py
@@ -1,5 +1,9 @@
-import pytest
+from unittest import mock
 
+import pytest
+import requests
+
+from telliot_core.gas import etherscan_gas
 from telliot_core.gas.etherscan_gas import EtherscanGasPrice
 from telliot_core.gas.etherscan_gas import EtherscanGasPriceSource
 
@@ -9,3 +13,31 @@ async def test_etherscan_gas():
     c = EtherscanGasPriceSource()
     result = await c.fetch_new_datapoint()
     assert isinstance(result[0], EtherscanGasPrice)
+
+
+@pytest.mark.asyncio
+async def test_etherscan_gas_error(caplog):
+    etherscan_gas.requests.get = mock.Mock(side_effect=requests.exceptions.ConnectionError)
+    c = EtherscanGasPriceSource()
+    result = await c.fetch_new_datapoint()
+    assert result[0] is None
+    assert result[1] is None
+    # assert "Connection timeout" in caplog.text
+
+    etherscan_gas.requests.get = mock.Mock(side_effect=requests.exceptions.Timeout)
+    c = EtherscanGasPriceSource()
+    result = await c.fetch_new_datapoint()
+    assert result[0] is None
+    assert result[1] is None
+    # assert "Timeout" in caplog.text
+
+    def invalid_json():
+        return "<p>blah<p/>"
+
+    etherscan_gas.requests.get = invalid_json
+    c = etherscan_gas.EtherscanGasPriceSource()
+    result = await c.fetch_new_datapoint()
+    assert result[0] is None
+    assert result[1] is None
+    # print(caplog.records)
+    # assert "JSONDecodeError" in caplog.text
