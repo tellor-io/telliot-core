17aa150fe07c9cf06cffa14cfca3f3fa57a3e512
akrem <akremstudy@gmail.com>
2023-02-17 10:23:55 -0500

    Fix arb id

--- src/telliot_core/gas/legacy_gas.py
+++ src/telliot_core/gas/legacy_gas.py
@@ -62,7 +62,7 @@ gas_station = {
     10: GasStation(api=OPTIMISM_GAS_PRICE_API, parse_rsp=["speeds", 2, "gasPrice"]),
     420: GasStation(api=OPTIMISM_GAS_PRICE_API, parse_rsp=["speeds", 2, "gasPrice"]),
     42161: GasStation(api=ARBITRUM_GAS_PRICE_API, parse_rsp=["speeds", 2, "gasPrice"]),
-    42163: GasStation(api=ARBITRUM_GAS_PRICE_API, parse_rsp=["speeds", 2, "gasPrice"]),
+    421613: GasStation(api=ARBITRUM_GAS_PRICE_API, parse_rsp=["speeds", 2, "gasPrice"]),
     137: GasStation(api=MATIC_GAS_PRICE_API, parse_rsp=["safeLow"]),
     80001: GasStation(api=MATIC_GAS_PRICE_API, parse_rsp=["safeLow"]),
     10200: GasStation(api=CHIADO_GAS_PRICE_API, parse_rsp=["average"]),
