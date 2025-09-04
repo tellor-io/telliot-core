98582e52427ccdfab8a7b3063e94144d7f2ff151
0xspuddy <sloetterle@tellor.io>
2025-09-04 13:04:52 -0400

    improved starting of hardhat node for actions test

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,23 @@ module.exports = {
   solidity: "0.8.19",
   networks: {
     hardhat: {
-      chainId: 1337
+      chainId: 1337,
+      host: "127.0.0.1",
+      port: 8545,
+      accounts: {
+        count: 10,
+        accountsBalance: "10000000000000000000000" // 10000 ETH
+      },
+      mining: {
+        auto: true,
+        interval: 1000
+      }
     }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./tests",
+    cache: "./cache",
+    artifacts: "./artifacts"
   }
 };
